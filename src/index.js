22c62d3caecfbc9a20909599da24afbea5f5b0fe
khabLexander <mantillagka@hotmail.com>
2022-11-18 15:11:29 -0500

    clock structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 import { Clock } from './components/Clock';
 
