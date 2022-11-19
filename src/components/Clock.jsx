e2edcc5206cd9535162bd01bdf92cad0cdf7be00
khabLexander <mantillagka@hotmail.com>
2022-11-19 10:53:15 -0500

    Refactorizando numeros key prop

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,12 +1,12 @@
 import { ClockHands } from './ClockHands';
-import { ClocksNumbers } from './ClocksNumbers';
+import { ClockNumbers } from './ClockNumbers';
 
 export const Clock = () => {
     
     return (
         <div className="clock">
             <ClockHands />
-            <ClocksNumbers/>
+            <ClockNumbers/>
         </div>
     )
 }
--- src/components/ClockNumbers.jsx
+++ src/components/ClockNumbers.jsx
@@ -1,4 +1,4 @@
-export const ClocksNumbers = () => {
+export const ClockNumbers = () => {
     
     const n = 12; 
 
